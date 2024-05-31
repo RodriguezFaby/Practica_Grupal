dee9f60e7ee8855a753df99de7ff6f0aa9a51e07
RodriguezFaby <f99rodriguez@outlook.com>
2024-05-31 16:59:07 -0600

    Primer commit

---
+++ Practica/Practica/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KN_Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Nos redirecciona al inicio de sesión
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Home()
+        {
+            return View();
+        }
+    }
+}
